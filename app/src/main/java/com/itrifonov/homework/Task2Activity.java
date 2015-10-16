1e913ce0d96e0fa33c84ba7682ef54664506084c
TrIgor74 <trigor74@gmail.com>
2015-10-16 17:39:20 +0300

    Added logic for fibonacci and factorial functions

--- app/src/main/java/com/itrifonov/homework/Task2Activity.java
+++ app/src/main/java/com/itrifonov/homework/Task2Activity.java
@@ -5,14 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Task2Activity extends AppCompatActivity {
 
-    public static long fibonacci (int n) {
-        //TODO add logic to calculate n-th element of fibonacci's sequence
-        return 0L;
+    public static long fibonacci(int n) {
+        if (n <= 0) return 0L;
+        else if (n == 1) return 1L;
+        else {
+            long f0 = 0L;
+            long f1 = 1L;
+            long result = 1L;
+            for (int i = 0; i < n - 1; i++) {
+                result = f0 + f1;
+                f0 = f1;
+                f1 = result;
+            }
+            return result;
+        }
     }
 
-    public static long factorial (int n) {
-        //TODO add logic to calculate n!
-        return 0L;
+    public static long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 
     @Override
