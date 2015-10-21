454df6092422bc436f940831e92d166fcec0236d
TrIgor74 <trigor74@gmail.com>
2015-10-21 13:58:02 +0300

    Added logic for task 3

--- app/src/main/java/com/itrifonov/homework/Task3Activity.java
+++ app/src/main/java/com/itrifonov/homework/Task3Activity.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 
-public class Task3Activity extends AppCompatActivity {
+public class Task3Activity extends AppCompatActivity implements View.OnClickListener {
 
     private String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
             "Blackberry", "Ubuntu", "Windows7", "Mac OS X", "Linux", "Ubuntu", "Windows7",
@@ -22,10 +26,10 @@ public class Task3Activity extends AppCompatActivity {
 
         createAndShowNumbers();
 
-        findViewById(R.id.button_1).setOnClickListener(clickListener);
-        findViewById(R.id.button_2).setOnClickListener(clickListener);
-        findViewById(R.id.button_3).setOnClickListener(clickListener);
-        findViewById(R.id.button_4).setOnClickListener(clickListener);
+        findViewById(R.id.button_1).setOnClickListener(this);
+        findViewById(R.id.button_2).setOnClickListener(this);
+        findViewById(R.id.button_3).setOnClickListener(this);
+        findViewById(R.id.button_4).setOnClickListener(this);
     }
 
     private void createAndShowNumbers() {
@@ -41,39 +45,64 @@ public class Task3Activity extends AppCompatActivity {
     }
 
     private void reverseOrder() {
+        TextView text2 = (TextView) findViewById(R.id.text_2);
 
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.addAll(Arrays.asList(values));
+        Collections.reverse(linkedList);
+
+        text2.setText(linkedList.toString());
     }
 
     private void removeEveryThird() {
+        TextView text2 = (TextView) findViewById(R.id.text_2);
 
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.addAll(Arrays.asList(values));
+
+        int i=2;
+        while (i < arrayList.size()) {
+            arrayList.remove(i);
+            i += 2;
+        }
+
+        text2.setText(arrayList.toString());
     }
 
     private void removeDuplicates() {
+        TextView text2 = (TextView) findViewById(R.id.text_2);
 
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.addAll(Arrays.asList(values));
+
+        text2.setText(hashSet.toString());
     }
 
     private void sortValues() {
+        TextView text2 = (TextView) findViewById(R.id.text_2);
 
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.addAll(Arrays.asList(values));
+        Collections.sort(linkedList, String.CASE_INSENSITIVE_ORDER);
+
+        text2.setText(linkedList.toString());
     }
 
-    private View.OnClickListener clickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.button_1:
-                    reverseOrder();
-                    break;
-                case R.id.button_2:
-                    removeEveryThird();
-                    break;
-                case R.id.button_3:
-                    removeDuplicates();
-                    break;
-                case R.id.button_4:
-                    sortValues();
-                    break;
-            }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_1:
+                reverseOrder();
+                break;
+            case R.id.button_2:
+                removeEveryThird();
+                break;
+            case R.id.button_3:
+                removeDuplicates();
+                break;
+            case R.id.button_4:
+                sortValues();
+                break;
         }
-    };
-
+    }
 }
