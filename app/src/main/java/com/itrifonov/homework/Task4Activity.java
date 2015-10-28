01e7835fd2575e623ca10f7214cc121cb28146eb
TrIgor74 <trigor74@gmail.com>
2015-10-29 00:50:27 +0200

    Added FragmentNavigation class

--- app/src/main/java/com/itrifonov/homework/Task4Activity.java
+++ app/src/main/java/com/itrifonov/homework/Task4Activity.java
@@ -1,10 +1,19 @@
 package com.itrifonov.homework;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
 public class Task4Activity extends BaseActivity {
 
     @Override
     protected boolean isAddFragment() {
         // TODO: 27.10.2015 add logic
-        return false;
+        return true;
+    }
+
+    @Override
+    protected void addFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.task4_main_layout, new NavigationFragment()).commit();
     }
 }
