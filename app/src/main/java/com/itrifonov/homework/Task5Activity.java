2dab3e5002b64fa9e28c331743163873bcefd70b
TrIgor74 <trigor74@gmail.com>
2015-11-08 23:05:40 +0200

    Added animation for task5

--- app/src/main/java/com/itrifonov/homework/Task5Activity.java
+++ app/src/main/java/com/itrifonov/homework/Task5Activity.java
@@ -6,11 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class Task5Activity extends AppCompatActivity {
+public class Task5Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private Animation animation;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +28,10 @@ public class Task5Activity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        String[] list = getResources().getStringArray(R.array.task5_item_list);
-        ListView listView = (ListView) findViewById(R.id.task5_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
-        listView.setAdapter(adapter);
+        findViewById(R.id.btn_animate).setOnClickListener(this);
+
+        imageView = (ImageView) findViewById(R.id.img_animate);
+        animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
     }
 
     @Override
@@ -40,21 +45,31 @@ public class Task5Activity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.animation1:
                 item.setChecked(true);
-                Toast.makeText(getApplicationContext(), "Item 1 Selected", Toast.LENGTH_LONG).show();
+                animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
                 return true;
             case R.id.animation2:
                 item.setChecked(true);
-                Toast.makeText(getApplicationContext(), "Item 2 Selected", Toast.LENGTH_LONG).show();
+                animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_down);
                 return true;
             case R.id.animation3:
                 item.setChecked(true);
-                Toast.makeText(getApplicationContext(), "Item 3 Selected", Toast.LENGTH_LONG).show();
+                animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
                 return true;
             case R.id.animation4:
                 item.setChecked(true);
-                Toast.makeText(getApplicationContext(), "Item 4 Selected", Toast.LENGTH_LONG).show();
+                animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.shake);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }      }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_animate:
+                imageView.setVisibility(View.VISIBLE);
+                imageView.startAnimation(animation);
+        }
+    }
 }
